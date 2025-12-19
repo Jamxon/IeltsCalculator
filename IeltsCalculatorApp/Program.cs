4c66a4897e9ff6ebe203497a94f908fe0d5d8d35
Jamxon <aliyevjamkhan499@gmail.com>
2025-12-19 18:55:30 +0500

    Init

---
+++ IeltsCalculatorApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using IeltsCalculatorService.Services;
+
+class Program
+{
+    static void Main()
+    {
+        double listening = GetScore("Listening");
+        double reading = GetScore("Reading");
+        double writing = GetScore("Writing");
+        double speaking = GetScore("Speaking");
+
+        IeltsCalculator calculator = new IeltsCalculator();
+
+        double overall = calculator.CalculateOverall(
+            listening, reading, writing, speaking
+        );
+
+        string category = calculator.GetCategory(overall);
+
+        Console.WriteLine("\n------------------");
+        Console.WriteLine($"Overall Score: {overall}");
+        Console.WriteLine($"Category: {category}");
+    }
+
+    static double GetScore(string skill)
+    {
+        Console.Write($"{skill} score: ");
+        return double.Parse(Console.ReadLine());
+    }
+}
---
+++ IeltsCalculatorService/Services/IeltsCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IeltsCalculatorService.Services
+{
+    public class IeltsCalculator
+    {
+        public double CalculateOverall(
+            double listening,
+            double reading,
+            double writing,
+            double speaking)
+        {
+            double average = (listening + reading + writing + speaking) / 4.0;
+
+            double floor = Math.Floor(average);
+            double diff = average - floor;
+
+            if (diff < 0.25)
+                return floor;
+            else if (diff < 0.75)
+                return floor + 0.5;
+            else
+                return floor + 1;
+        }
+
+        public string GetCategory(double score)
+        {
+            if (score == 9)
+                return "Expert";
+            else if (score >= 8)
+                return "Very Good";
+            else if (score >= 7)
+                return "Good";
+            else if (score >= 6)
+                return "Competent";
+            else if (score >= 5)
+                return "Modest";
+            else
+                return "Limited";
+        }
+    }
+}
